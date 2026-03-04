669ca94240317520afae58289e16817e9d782d65
24zhangzichen24 <zhangzichen703@gmail.com>
2026-03-04 15:15:19 -0600

    update ball array

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,6 +4,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
@@ -15,21 +16,36 @@ function draw() {
     ball.y += ball.dy;
 
     //display
+    fill(ball.color);
     circle(ball.x, ball.y, ball.radius * 2);
+    // teleport
+    if (ball.x + ball.radius < 0 ) {
+      ball.x = width + ball.radius;
+    }
+    if (ball.x - ball.radius > width) {
+      ball.x = -ball.radius;
+    }
+    if (ball.y + ball.radius < 0 ) {
+      ball.y = height + ball.radius;
+    }
+    if (ball.y - ball.radius > height) {
+      ball.y = -ball.radius;
+    }
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let theBall = {
-    x: random(width),
-    y: random(height),
+    x: _x !== undefined ? _x : random(width),
+    y: _y !== undefined ? _y : random(height),
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 40),
+    color: color(random(255), random(255), random(255))
   };
   ballArray.push(theBall);
 }
